ecbdcb6d81c6bbbb99d35a1a7afe99bf24bffd72
beardm4n <dmitriykorovko@gmail.com>
2023-05-20 22:30:21 +0300

    create a loop to get updates, add env

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module tg_bot
 
 go 1.19
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
 )
 
 const (
-	BASE_URL  = "https://api.telegram.org/bot"
-	BOT_TOKEN = "6253089031:AAHZPs-z7R5TyctRaAnpUWHDo8EoC_2TbxQ"
+	BASE_URL = "https://api.telegram.org/bot"
 )
 
 var (
@@ -17,16 +22,37 @@ var (
 		"GET_UPDATES":  "getUpdates",
 		"SEND_MESSAGE": "sendMessage",
 	}
-	offset = 0
+	offset   = 0
+	botToken string
 )
 
 func main() {
+	err := initEnv()
+	if err != nil {
+		log.Fatal("Error in loadEnv method .env: ", err)
+	}
+
+	apiUrl := BASE_URL + botToken + "/" + telegramMethods["GET_UPDATES"]
+
+	for {
+		updates, err := getUpdates(apiUrl, offset)
+		if err != nil {
+			log.Println("Something went wrong in getUpdates", err.Error())
+		}
+
+		for _, update := range updates {
+			offset = update.UpdateId + 1
+		}
+
+		fmt.Printf("Updates: %v\n", updates)
+
+		// Пауза между запросами на получение обновлений
+		time.Sleep(time.Second)
+	}
 }
 
-func getUpdates(baseUrl string, botToken string, apiMethod string) ([]Update, error) {
-	apiUrl := baseUrl + botToken + "/" + apiMethod
-
-	resp, err := http.Get(apiUrl)
+func getUpdates(apiUrl string, offset int) ([]Update, error) {
+	resp, err := http.Get(apiUrl + "?offset=" + strconv.Itoa(offset))
 	if err != nil {
 		fmt.Println("Something went wrong in requset:", err)
 		return nil, err
@@ -52,5 +78,17 @@ func getUpdates(baseUrl string, botToken string, apiMethod string) ([]Update, er
 		fmt.Println("Something went wrong in parse json:", err)
 		return nil, err
 	}
+	
 	return restResponse.Result, nil
+}
+
+func initEnv() error {
+	err := godotenv.Load()
+	if err != nil {
+		return err
+	}
+
+	botToken = os.Getenv("BOT_TOKEN")
+
+	return nil
 }
