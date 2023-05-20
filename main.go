61544ca678e70859944ab43afe850157a70735da
beardm4n <dmitriykorovko@gmail.com>
2023-05-20 03:50:37 +0300

    add local server

--- main.go
+++ main.go
@@ -1,91 +1,54 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"strconv"
-)
-
-const (
-	botToken          = "<somekey>"
-	botApi            = "https://api.telegram.org/bot"
-	getUpdatesMethod  = "getUpdates"
-	sendMessageMethod = "sendMessage"
 )
 
 func main() {
-	requestBorUrl := botApi + botToken
-	offset := 0
+	http.HandleFunc("/", handleRequest)
 
-	for {
-		updates, err := getUpdates(requestBorUrl, offset)
-		if err != nil {
-			log.Println("Something went wrong in getUpdates", err.Error())
-		}
+	// Укажите порт, на котором будет запущен сервер
+	port := "8080"
 
-		for _, update := range updates {
-			err := respond(requestBorUrl, update)
-			if err != nil {
-				log.Println("Something went wrong in update", err.Error())
-			}
-			offset = update.UpdateId + 1
-		}
+	log.Printf("Сервер запущен на http://localhost:%s\n", port)
 
-		fmt.Println(updates)
+	// Запустите сервер на указанном порту
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal("Ошибка при запуске сервера:", err)
 	}
 }
 
-// получение обновлений
-func getUpdates(botUrl string, offset int) ([]Update, error) {
-	resp, err := http.Get(botUrl + "/" + getUpdatesMethod + "?offset=" + strconv.Itoa(offset))
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Временно наживать F5 для получения обновлений")
+	// Отправьте GET-запрос на метод getUpdates Телеграм API
+	response, err := http.Get("https://api.telegram.org/bot6190524855:AAFug058VCgIcqfmgyKiXq8j5GhQz3yEL-M/getUpdates")
 	if err != nil {
-		return nil, err
+		log.Println("Ошибка при получении обновлений:", err)
+		return
+	}
+	defer response.Body.Close()
+
+	// Прочитайте ответ в виде JSON-данных
+	var result struct {
+		Ok     bool     `json:"ok"`
+		Result []Update `json:"result"`
+	}
+	err = json.NewDecoder(response.Body).Decode(&result)
+	if err != nil {
+		log.Println("Ошибка при чтении ответа:", err)
+		return
 	}
 
-	// закрываем запрос после того как выйдем из функции
-	defer resp.Body.Close()
-
-	// ответ от сервера получаем в байтах, необходимо обработать его
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
+	// Обработайте полученные обновления
+	for _, update := range result.Result {
+		// Выполните необходимые действия с каждым обновлением
+		log.Printf("Получено обновление: %+v\n", update)
 	}
 
-	var restResponse RestResponse
-
-	// необходим распарсить json, который получили от сервера, который приведем к структуре RestResponse
-	err = json.Unmarshal(body, &restResponse)
-	if err != nil {
-		return nil, err
-	}
-
-	return restResponse.Result, nil
-}
-
-// ответ на обновления
-func respond(botUrl string, update Update) error {
-	var botMessage BotMessage
-
-	botMessage.ChatId = update.Message.Chat.Id
-	botMessage.Text = update.Message.Text
-
-	// тело запроса передается в байтовом формате
-	buf, err := json.Marshal(botMessage)
-	if err != nil {
-		return err
-	}
-
-	url := botUrl + "/" + sendMessageMethod
-
-	// для того, чтобы передать тело запроса нужен Reader, при помощи bytes.NewBuffer создаем его
-	_, err = http.Post(url, "application/json", bytes.NewBuffer(buf))
-	if err != nil {
-		return err
-	}
-
-	return nil
+	// Отправьте ответ клиенту
+	w.WriteHeader(http.StatusOK)
 }
