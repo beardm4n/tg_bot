664be67ae885987ebdb0f1bbf4954d9f9c0b0f42
beardm4n <dmitriykorovko@gmail.com>
2023-05-21 12:00:17 +0300

    method transfer ti utils

--- main.go
+++ main.go
@@ -1,17 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"strconv"
 	"time"
-)
-
-const (
-	BASE_URL = "https://api.telegram.org/bot"
 )
 
 var (
@@ -21,27 +12,31 @@ var (
 	}
 	offset   = 0
 	botToken string
+	baseUrl  string
 )
 
 func main() {
 	/*
-		 Загружаем переменные окружения
-		 Внутри метода можно инициализировать переменную, которую берем из env (но сначала надо ее определить в const/var)
+	 Загружаем переменные окружения
+	 Внутри метода можно инициализировать переменную, которую берем из env (но сначала надо ее определить в var)
 	*/
 	err := initEnv()
 	if err != nil {
-		log.Fatal("Error in loadEnv method: ", err)
+		fmt.Println("Error in loadEnv method: ", err)
+		return
 	}
 
-	apiUrl := BASE_URL + botToken + "/" + telegramMethods["GET_UPDATES"]
+	apiUrl := baseUrl + botToken + "/" + telegramMethods["GET_UPDATES"]
 
 	for {
 		updates, err := getUpdates(apiUrl, offset)
 		if err != nil {
-			log.Println("Something went wrong in getUpdates: ", err)
+			fmt.Println("Something went wrong in getUpdates: ", err)
 		}
 
 		for _, update := range updates {
+			checkMainCommand(update.Message)
+			
 			offset = update.UpdateId + 1
 		}
 
@@ -51,35 +46,3 @@ func main() {
 		time.Sleep(time.Second)
 	}
 }
-
-func getUpdates(apiUrl string, offset int) ([]Update, error) {
-	resp, err := http.Get(apiUrl + "?offset=" + strconv.Itoa(offset))
-	if err != nil {
-		fmt.Println("Something went wrong in requset: ", err)
-		return nil, err
-	}
-
-	// ответ от сервера получаем в байтах, необходимо обработать его
-	body, err := io.ReadAll(resp.Body)
-
-	defer resp.Body.Close()
-
-	if err != nil {
-		if err != nil {
-			fmt.Println("Something went wrong in error handling: ", err)
-			return nil, err
-		}
-	}
-
-	var restResponse RestResponse
-
-	// необходим распарсить json, который получили от сервера, который приведем к структуре RestResponse
-	err = json.Unmarshal(body, &restResponse)
-	if err != nil {
-		fmt.Println("Something went wrong in parse json: ", err)
-		return nil, err
-	}
-
-	return restResponse.Result, nil
-}
-
--- utils.go
+++ utils.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -13,6 +18,48 @@ func initEnv() error {
 	}
 
 	botToken = os.Getenv("BOT_TOKEN")
+	baseUrl = os.Getenv("BASE_URL")
 
 	return nil
+}
+
+func getUpdates(apiUrl string, offset int) ([]Update, error) {
+	resp, err := http.Get(apiUrl + "?offset=" + strconv.Itoa(offset))
+	if err != nil {
+		fmt.Println("Something went wrong in requset: ", err)
+		return nil, err
+	}
+
+	// ответ от сервера получаем в байтах, необходимо обработать его
+	body, err := io.ReadAll(resp.Body)
+
+	defer resp.Body.Close()
+
+	if err != nil {
+		if err != nil {
+			fmt.Println("Something went wrong in error handling: ", err)
+			return nil, err
+		}
+	}
+
+	var restResponse RestResponse
+
+	// необходим распарсить json, который получили от сервера, который приведем к структуре RestResponse
+	err = json.Unmarshal(body, &restResponse)
+	if err != nil {
+		fmt.Println("Something went wrong in parse json: ", err)
+		return nil, err
+	}
+
+	return restResponse.Result, nil
+}
+
+func checkMainCommand(message Message) {
+	if message.Text == "/start" {
+		fmt.Println("Hi, nice to meet you!")
+	} else if message.Text == "" {
+		fmt.Println("Bye bye! see you soon")
+	} else {
+		fmt.Println("Unknow command =/")
+	}
 }
